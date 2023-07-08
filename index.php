0e919da691bc64e72b84da6c78dee44fb799cf26
Tobiascorreia <138892313+Tobiascorreia@users.noreply.github.com>
2023-07-08 11:56:56 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php include 'inc/cabecalho.inc.php';?>
+     
+
+    <div class="corpo">
+        
+    <section class="menu"> 
+        <h2> <strong> Cardápio </strong></h2>
+        <nav> <h2>
+            <li><a href="#">Hotduck</a></li>
+            <li><a href="#">X-Pato</a></li>
+            <li><a href="#">Kingburguer</a></li>
+            <li><a href="#">X-Metralha</a></li>
+            <li><a href="#">Kingjúnior</a></li>
+            <li><a href="#">X-patinha</a></li></h2>
+        </nav>
+    </section>
+    <section class="destaque"> </section>
+    </div>
+
+<?php include 'inc/rodape.inc.php';?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sobre.php
@@ -0,0 +1,42 @@
+<?php include "inc/cabecalho.inc.php";?>
+<div class="fundo">
+        <h1>kingdonalds</h1>
+        <p> <strong> kingdonalds </strong> é uma empressa do genero alimenticio fast food, que tem por objetivo trazer para o mercado os mais <mark>saborosos lanches
+        </mark>, fazendo uma mescla com que tem de melhor em seu concorrente burger king e mc donalds</p>
+        <p> 
+            a <b><em>matriz</em></b> fica localizada na cidade  de ponta gorassa, no estado do parana.somente na cidade temos a matriz e <del> 8 filiais</del>
+           <sup>10</sup> filiais, sao mais de 250 funcionarios que fazem as engrenagens de nossa empresa girar <small>mais forte
+           </small>
+        </p>
+        <figure class="matriz"> 
+            <img src="img/matriz.jpg" alt="Matriz kingdonald's">
+            <figcaption> Matriz king donald's</figcaption>
+        </figure>
+<p>
+toda nossa materia prima é exportados dos mais variados paises, sempre priporizanda da maior qualidade. foram realizadas 
+diversas pesquisas para validas as melhores materias primas e serem utilizadas e
+aproximadamente 7 paises diferentes  importados tais materias primas.
+</p>
+<h2>Historia</h2>
+<figure class="funcionarios">
+    <img src="img/download (1).jpeg" alt="download (1).jpeg">
+</figure>
+<p>fundadores do kingdonalds
+    <ul>
+    <li>faustao</li>
+    <li>Silvio santos</li>
+    <li>Augustinho </li>
+    <li>Uliana</li>
+    </ul>
+</p>
+<h2>Diferenciais</h2>
+<ol>
+    <li>produtos organicos</li>
+    <li>Formula do hamburguer do siri</li>
+    <li>representando</li>
+</ol>
+<?php include "inc/rodape.inc.php";?>
+></div>
+
+
+   
